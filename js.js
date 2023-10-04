8a1ce91afa82ee9a6aac31015aff3f85e1558d21
CTC CCP 7 <ctcccp7@CTCs-Air.cape.k12.mo.us>
2023-10-04 14:27:04 -0500

    first commit

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
-const mydiv = document.getElementById("myDiv")
+const myDiv = document.getElementById("myDiv")
 let p1 = document.getElementById("thisId1").innerText;
 let p2 = document.getElementById("thisId2").innerText;
 
-mydiv.style.background = "lightblue";
+myDiv.style.background = "lightblue";
 document.getElementById("myDiv").style.background = "lightblue";
 console.log(p1, p2);
 
