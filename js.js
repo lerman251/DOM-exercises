3361c40be6662840a1d29555af031f50b4b64122
CTC CCP 7 <ctcccp7@CTCs-Air.cape.k12.mo.us>
2023-10-04 14:22:20 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,14 @@
+const mydiv = document.getElementById("myDiv")
+let p1 = document.getElementById("thisId1").innerText;
+let p2 = document.getElementById("thisId2").innerText;
+
+mydiv.style.background = "lightblue";
+document.getElementById("myDiv").style.background = "lightblue";
+console.log(p1, p2);
+
+
+
+
+
+
+
